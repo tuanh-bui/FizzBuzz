477afdf2e2a203faa28559a56f5dc77b1479ff16
tuanh-bui <Luongtuanhbui@gmail.com>
2025-09-10 23:16:32 -0400

    implemenation of Multiples.multiples with passed tests

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,22 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+
+    }
+
+    static int multiples(int n, int a, int b) {
+        int count = 0;
+        int i = 1;
+        while (i < n){
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                count++;
+            }
+            i ++;
+        }
+        return count;
+    }
+}
